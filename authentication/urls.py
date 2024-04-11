4053da934767620bfc91a1581ea743b2b8824bcf
virajpatel10 <patelviraj0170@gmail.com>
2024-04-11 15:18:07 -0400

    signup

--- authentication/urls.py
+++ authentication/urls.py
@@ -4,8 +4,8 @@ from django.views.decorators.csrf import csrf_exempt
 
 
 urlpatterns = [
-    path('signup', RegistrationView.as_view(), name="signup"),
-    path('login', LoginView.as_view(), name="login"),
+    path('signup', RegistrationView.as_view(), name="Register"),
+    path('login', LoginView.as_view(), name="Login"),
     path('logout', LogoutView.as_view(), name="logout"),
     path('validate-username', csrf_exempt(UsernameValidationView.as_view()),
          name="validate-username"),
